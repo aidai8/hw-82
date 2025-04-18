b9692361a2f23ef3da4f0165da5b16b6c221dbd8
Aidai Mashrapova <aidaimashrapova8@mail.ru>
2025-04-18 16:16:21 +0600

    [HW-83] Track History model and routes logic added

--- models/TrackHistory.ts
+++ models/TrackHistory.ts
@@ -3,7 +3,21 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const TrackHistorySchema = new Schema({
-
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    track: {
+        type: Schema.Types.ObjectId,
+        ref: "Track",
+        required: true,
+    },
+    datetime: {
+        type: Date,
+        default: Date.now,
+        required: true,
+    },
 });
 
 const TrackHistory = mongoose.model("TrackHistory", TrackHistorySchema);
