bc5b3729525b751e2fd5ab13653311921e361d90
Aidai Mashrapova <aidaimashrapova8@mail.ru>
2025-04-18 15:50:21 +0600

    [HW-83] Users model and routes logic added

--- models/TrackHistory.ts
+++ models/TrackHistory.ts
@@ -3,19 +3,7 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const TrackHistorySchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    token: {
-        type: String,
-        required: true,
-    }
+
 });
 
 const TrackHistory = mongoose.model("TrackHistory", TrackHistorySchema);
--- models/User.ts
+++ models/User.ts
@@ -1,5 +1,7 @@
 import mongoose, {HydratedDocument, Model} from "mongoose";
 import {UserFields} from "../types";
+import {randomUUID} from "node:crypto";
+import bcrypt from "bcrypt";
 
 interface UserMethods {
     checkPassword: (password: string) => Promise<boolean>;
@@ -28,6 +30,32 @@ const UserSchema = new mongoose.Schema<
     token: {
         type: String,
         required: true,
+    }
+});
+
+UserSchema.methods.checkPassword = async function (password: string) {
+    const user = this;
+    return bcrypt.compare(password, user.password);
+}
+
+UserSchema.methods.generateToken = function () {
+    this.token = randomUUID();
+}
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified("password")) return next();
+
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    const hash = await bcrypt.hash(this.password, salt);
+
+    this.password = hash;
+    next();
+});
+
+UserSchema.set("toJSON", {
+    transform: (doc, ret) => {
+        delete ret.password;
+        return ret;
     }
 });
 
